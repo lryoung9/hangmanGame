6dbf7d9aedfd206de46789b45c236bd820fd374c
Lydia Young <lydiaryoung@gmail.com>
2018-01-23 22:08:17 -0700

    button function to reveal hint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
 // Variables
 var wins = 0; // # of times the user has guessed a word correctly
 var losses = 0; // # of times the user has used up all attempts before guessing the word ("hanging")
-var hanging = {head, body, lfArm, rtArm, lfLeg, rtLeg}; // guess left related to "body parts" left to hang
-var guessesLeft = hanging.length;
+var guessesLeft = 6; // guess left related to "body parts" left to hang
 var userGuesses = []; // an array of the letters that the user typed. Display these until the user either wins or loses.
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //array of letters the computer can choose from
 // Array containing possible movies referenced and the word to guess
@@ -13,6 +12,13 @@ var words = [
   {movieName: "James and the Giant Peach", movieRef: ["orphan", "grasshopper", "centipede", "ladybuy", "glowworm", "spider", "peach"]},
   {movieName: "Sweeny Todd", movieRef: ["barber", "London", "straight razor", "serial killer", "victorian", "meat pie"]},
 ];
+// Randomly select movie
+var movie = words[Math.floor(Math.random()*words.length)];
+console.log(movie);
+var hint = movie.movieName;
+// Randomly select word from the movie's movieRef
+var word = movie.movieRef[Math.floor(Math.random()*movie.movieRef.length)];
+// Reset game after win or loss
 function resetGame() {
 	console.log("-----------------");
 	console.log("Game Reset");
@@ -39,14 +45,16 @@ function resetGame() {
     reference.innerHTML = "Hint: " + movie;
 };
 
-// Randomly select movie
-var movie = words[Math.floor(Math.random()*words.length)];
-console.log(movie);
-// Randomly select word from the movie's movieRef
-var word = movie.movieRef[Math.floor(Math.random()*movie.movieRef.length)];
-console.log(hint);
-    var reference = document.querySelector("hint");
-    // Give hint as to what Movie the word is related to (words properties)
-    reference.innerHTML = "Hint: " + movie;
-};
+// If hint button is clicked, give hint as to what Movie the word is related to (words properties)
+document.addEventListener("click", function(){
+    document.getElementById("hint").innerHTML = movie.movieName;
+});
+
+document.onkeyup = function(event) {
+
+}
+
+
+
+
 
